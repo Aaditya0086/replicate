b45578040eb21fac106a87e3b48a813f8e7c50ac
aaditya <gargaaditya.18@gmail.com>
2024-03-07 22:39:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+import MainDesign from "./components/design/MainDesign.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <Router>
+          {/* <Navbar refToScrollCertifications={scrollToComponentRefCe} refToScrollProducts={scrollToComponentRefP} refToScrollAbout={scrollToComponentRefA} refToScrollContact={scrollToComponentRefCo} refToScrollMap={scrollToComponentRefM} /> */}
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<MainDesign/>}
+            />
+
+            {/* <Route exact path="/members" element={<Members />} /> */}
+          </Routes>
+        </Router>
+      </>
     </div>
   );
 }
---
+++ src/components/design/MainDesign.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+const MainDesign = () => {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default MainDesign
